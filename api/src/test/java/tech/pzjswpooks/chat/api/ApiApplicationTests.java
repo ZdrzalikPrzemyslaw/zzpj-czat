351c3db3649aae947a31ba0154851a73bf516c2c
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2021-04-05 21:36:16 +0200

    init folder

---
+++ api/src/test/java/tech/pzjswpooks/chat/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.pzjswpooks.chat.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
