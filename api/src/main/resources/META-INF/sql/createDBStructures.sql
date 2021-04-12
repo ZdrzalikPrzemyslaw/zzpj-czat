0761e367b7bdd7ea97a956e355685b48d6081de3
224441 Szymańska Julia <szymanska.julia.it@gmail.com>
2021-04-12 21:54:09 +0200

    Started working on account table in sql structure

--- api/src/main/resources/META-INF/sql/createDBStructures.sql
+++ api/src/main/resources/META-INF/sql/createDBStructures.sql
@@ -1,1 +1,12 @@
---  TODO: stworzyć struktury bazy danych
+DROP TABLE IF EXISTS chats;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE account
+(
+    id BIGINT PRIMARY KEY,
+    username VARCHAR(32) UNIQUE NOT NULL,
+    -- CHAR(64) - skrot hasla bedzie mial stala dlugosc - 64 znkai
+    password CHAR(64) NOT NULL,
+
+)
