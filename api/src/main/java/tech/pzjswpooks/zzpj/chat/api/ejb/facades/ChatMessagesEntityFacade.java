97476d9902b26fd836b81d7c64dcabec17598c6a
Yarvi <Kamilkamil1@poczta.onet.pl>
2021-04-30 16:39:48 +0200

    Added facade for ChatMessages

---
+++ api/src/main/java/tech/pzjswpooks/zzpj/chat/api/ejb/facades/ChatMessagesEntityFacade.java
@@ -0,0 +1,68 @@
+package tech.pzjswpooks.zzpj.chat.api.ejb.facades;
+
+import tech.pzjswpooks.zzpj.chat.api.entities.AccountsEntity;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
+/**
+ * Klasa definiująca główne operacje wykonywane na encjach typu AccountEntity.
+ */
+@Stateless
+// TODO: 24.04.2021 Nie jestem pewien czy tutaj ta tranzakcja jest konieczna
+@TransactionAttribute(TransactionAttributeType.MANDATORY)
+public class AccountEntityFacade extends AbstractFacade<AccountsEntity> {
+
+    @PersistenceContext(unitName = "zzpjadmin")
+    private EntityManager em;
+
+    /**
+     * Tworzy nową instancję klasy AccountFacade.
+     */
+    public AccountEntityFacade() {
+        super(AccountsEntity.class);
+    }
+
+    /**
+     * Find by username account.
+     *
+     * @param username username
+     * @return account
+     */
+    public AccountsEntity findByUsername(String username) {
+        TypedQuery<AccountsEntity> tq = em.createNamedQuery("AccountsEntity.findByUsername", AccountsEntity.class);
+        tq.setParameter("username", username);
+        return tq.getSingleResult();
+    }
+
+    /**
+     * Tworzy nową instancję klasy AccountFacade.
+     *
+     * @param entityClass entity class
+     */
+    public AccountEntityFacade(Class<AccountsEntity> entityClass) {
+        super(entityClass);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    @Override
+    public void edit(AccountsEntity entity)  {
+        try {
+            super.edit(entity);
+        } catch (PersistenceException e) {
+            throw e;
+            // TODO: 20.04.2021 - uzupełnić o wyjątki aplikacyjne
+        }
+    }
+
+
+}
