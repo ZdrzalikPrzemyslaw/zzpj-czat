e836d27f92d6644347918689fa36e8b4e489d0aa
oskarolaszczyk <oskarolaszczyk@gmail.com>
2021-05-04 08:52:44 +0200

    added facade file

---
+++ api/src/main/java/tech/pzjswpooks/zzpj/chat/api/ejb/facades/ChatsEntityFacade.java
@@ -0,0 +1,71 @@
+package tech.pzjswpooks.zzpj.chat.api.ejb.facades;
+
+import tech.pzjswpooks.zzpj.chat.api.entities.ChatsEntity;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
+/**
+ * Klasa definiująca główne operacje wykonywane na encjach typu ChatsEntity.
+ */
+@Stateless
+// TODO: 24.04.2021 Nie jestem pewien czy tutaj ta tranzakcja jest konieczna
+@TransactionAttribute(TransactionAttributeType.MANDATORY)
+public class ChatsEntityFacade extends AbstractFacade<ChatsEntity> {
+
+    @PersistenceContext(unitName = "zzpjadmin")
+    private EntityManager em;
+
+    /**
+     * Tworzy nową instancję klasy ChatsEntity.
+     */
+    public ChatsEntityFacade() {
+        super(ChatsEntity.class);
+    }
+
+    /**
+     * Find by id chat.
+     *
+     * @param id id
+     * @return chat
+     */
+    public ChatsEntity findById(Long id) {
+        TypedQuery<ChatsEntity> tq = em.createNamedQuery("ChatsEntity.findById", ChatsEntity.class);
+        tq.setParameter("id", id);
+        return tq.getSingleResult();
+    }
+
+    /**
+     * Find all chats.
+     *
+     * @return chats
+     */
+    public ChatsEntity findAll() {
+        TypedQuery<ChatsEntity> tq = em.createNamedQuery("ChatsEntity.findAll", ChatsEntity.class);
+        return tq.getResultList();
+    }
+
+
+    /**
+     * Tworzy nową instancję klasy ChatsFacade.
+     *
+     * @param entityClass entity class
+     */
+    public ChatsEntityFacade(Class<ChatsEntity> entityClass) {
+        super(entityClass);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+
+
+
+}
