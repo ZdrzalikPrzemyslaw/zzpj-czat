21a59e263c0e3e7b08de41e88cc99dac5da4066b
Yarvi <Kamilkamil1@poczta.onet.pl>
2021-05-10 12:32:55 +0200

    Changed parameters to not required

--- api/src/main/java/tech/pzjswpooks/zzpj/chat/api/payloads/request/ChangeUserRequestDto.java
+++ api/src/main/java/tech/pzjswpooks/zzpj/chat/api/payloads/request/ChangeUserRequestDto.java
@@ -1,31 +1,25 @@
 package tech.pzjswpooks.zzpj.chat.api.payloads.request;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import tech.pzjswpooks.zzpj.chat.api.entities.AccountsEntity;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class ChangeUserRequestDto {
-    @NotNull
     @Email(message = "Not an email")
     @Size(max = 100, message = "Too long email")
     private String email;
-    @NotEmpty
     @Size(max = 50, message = "Too long first name")
     private String firstName;
-    @NotEmpty
     @Size(max = 80, message = "Too long last name")
     private String lastName;
     private String language;
     private String phoneNumber;
 
     public ChangeUserRequestDto(
-                                @NotNull @Email(message = "Not an email") @Size(max = 100, message = "Too long email") String email,
-                                @NotEmpty @Size(max = 50, message = "Too long first name") String firstName,
-                                @NotEmpty @Size(max = 80, message = "Too long last name") String lastName,
+                                @Email(message = "Not an email") @Size(max = 100, message = "Too long email") String email,
+                                @Size(max = 50, message = "Too long first name") String firstName,
+                                @Size(max = 80, message = "Too long last name") String lastName,
                                 String language, String phoneNumber) {
         this.email = email;
         this.firstName = firstName;
