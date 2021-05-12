a943587ab66f2ae5901d602e8fdd6b31524d6ace
WitoldPietrzak <witekpietrzak35@gmail.com>
2021-05-12 21:54:27 +0200

    Removed finById from AccessLevelsFacade

--- api/src/main/java/tech/pzjswpooks/zzpj/chat/api/ejb/facades/AccessLevelsFacade.java
+++ api/src/main/java/tech/pzjswpooks/zzpj/chat/api/ejb/facades/AccessLevelsFacade.java
@@ -32,12 +32,6 @@ public class AccessLevelsFacade extends AbstractFacade<AccessLevelsEntity> {
         return em;
     }
 
-    public AccessLevelsEntity findById(String id) {
-        TypedQuery<AccessLevelsEntity> tq = em.createNamedQuery("AccessLevelsEntity.findById", AccessLevelsEntity.class);
-        tq.setParameter("id", id);
-        return tq.getSingleResult();
-    }
-
     public List<AccessLevelsEntity> findByUsername(String username) {
         TypedQuery<AccessLevelsEntity> tq = em.createNamedQuery("AccessLevelsEntity.findByUsername", AccessLevelsEntity.class);
         tq.setParameter("username", username);
