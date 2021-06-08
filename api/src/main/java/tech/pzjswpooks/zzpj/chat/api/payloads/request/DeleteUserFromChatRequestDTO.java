6e7f36e00170faf2400ab6c3e10b18af44c4b666
oskarolaszczyk <oskarolaszczyk@gmail.com>
2021-06-09 01:23:14 +0200

    added delete request dto

---
+++ api/src/main/java/tech/pzjswpooks/zzpj/chat/api/payloads/request/DeleteUserFromChatRequestDTO.java
@@ -0,0 +1,26 @@
+package tech.pzjswpooks.zzpj.chat.api.payloads.request;
+
+import tech.pzjswpooks.zzpj.chat.api.security.Login;
+
+import javax.validation.constraints.NotEmpty;
+
+public class DeleteUserFromChatRequestDTO {
+    @NotEmpty
+    @Login
+    private String username;
+
+    public DeleteUserFromChatRequestDTO(@NotEmpty String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public DeleteUserFromChatRequestDTO() {
+    }
+}
